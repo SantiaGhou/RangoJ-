4b0d5b0ad64b56a8953e1d521fb6ba6598693f18
Filipe Santiago Santos <filipecacule@gmail.com>
2025-06-06 10:30:41 -0300

    Header added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Logo from '../../assets/RangoJa.png';
+import styles from './Header.module.css';
+
+export function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        if (!isMenuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+        document.body.style.overflow = 'unset';
+    };
+
+    return(
+        <header className={styles.header}>
+            <img src={Logo} alt="Logo" />
+            <nav className={styles.menu}>
+                <a href="#" onClick={closeMenu}>Funcionalidades</a>
+                <a href="#" onClick={closeMenu}>Nossos Clientes</a>
+                <a href="#" onClick={closeMenu}>Catalogo</a>
+            </nav>
+            <div className={styles.buttons}>
+                <a href="#" className={styles.createAccountLink} onClick={closeMenu}>
+                    criar conta
+                </a>
+                <button className={styles.loginButton} onClick={closeMenu}>Entrar</button>
+            </div>
+            <div className={`${styles.hamburger} ${isMenuOpen ? styles.open : ''}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.open : ''}`}>
+                <a href="#" onClick={closeMenu}>Carreiras</a>
+                <a href="#" onClick={closeMenu}>iFood Card</a>
+                <a href="#" onClick={closeMenu}>Para Empresas</a>
+                <div className={styles.mobileButtons}>
+                    <a href="#" className={styles.createAccountLink} onClick={closeMenu}>
+                        criar conta
+                    </a>
+                    <button className={styles.loginButton} onClick={closeMenu}>Entrar</button>
+                </div>
+            </div>
+        </header>
+    )
+}
